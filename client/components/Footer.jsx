52e96adae605ce44c9239c8afe8baa094fbb17fd
EDA <eda@mac-mini-2.local>
2017-01-20 12:20:24 +1300

    Added basic header and footer html

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -4,6 +4,7 @@ export default React.createClass({
   render () {
     return (
       <div className='footer'>
+        <p>Created by Amanda Hogan, Anna Jackson, Anna Ulyanova and Jessica Neary &copy; 2017</p>
       </div>
     )
   }
--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 export default React.createClass({
   render () {
     return (
       <div className='header'>
+        <Link to={'/'} ><img src={'../../images/EDAlogo.png'} /></Link>
         <h1>Welcome to EDA's Library of Things</h1>
       </div>
     )
